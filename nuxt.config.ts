1b9b4918149ead0a5b40d0a05533cfafe1d0bb7d
Jaromil90 <manuel.detoma@noaignite.com>
2024-06-04 09:46:29 +0200

    add dirs imports directive on nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  imports: {
+    dirs: ["constants", "mocks", "types"],
+  },
   modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
 })
